e4b2e9f3fcb0b5c5977e84ecd91c1347c7fe89c9
kanzakiy <kanzakiy@ucr.edu>
2019-02-02 18:32:45 -0800

    PyWeath

--- STc.sh
+++ STc.sh
@@ -3,12 +3,21 @@ a=1
 c=1
 while [ $a -lt 12 ]
 do 
-  echo $a
+  # echo $a
   b=1
   while [ $b -lt 12 ]
   do 
-    echo $b
+    # echo $b
+    echo $c
+    # if [ $c -eq 10 ]||[ $c -eq 15 ]||[ $c -eq 16 ]||[ $c -eq 14 ]||[ $c -eq 18 ]||[ $c -eq 19 ]||[ $c -eq 27 ]; then 
+      # b=`expr $b + 1`
+      # c=`expr $c + 1`
+      # continue
+    # fi
     gfortran -cpp -Dpyweath -Dvar1=${a} -Dvar2=${b} -o run${c} o2profile+silweath+o2_v9_7.f -lopenblas -g -fcheck=all 
+    if [[ -f ~/./run/run${c} ]]; then
+      rm ./run/run${c}
+    fi
     cp run${c} ./run
     rm run${c}
     b=`expr $b + 1`
