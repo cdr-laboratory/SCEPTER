da965b2f916a36a04a051597e9bc083cfbcfb513
Yoshiki Kanzaki <ykanzaki3@login-phoenix-3.pace.gatech.edu>
2022-04-21 03:04:10 -0400

    adding pioneer basalt

--- data/basalt_defines.h
+++ data/basalt_defines.h
@@ -1,8 +1,8 @@
 ! user-define  basalt_chem_cmp
 #define def_bas_si_fr 1.0
-#define def_bas_al_fr 0.360297048
-#define def_bas_fe2_fr 0.23509036
-#define def_bas_mg_fr 0.188097811
-#define def_bas_ca_fr 0.203346684
-#define def_bas_na_fr 0.1042637
-#define def_bas_k_fr 0.020287583
+#define def_bas_al_fr 0.311418127
+#define def_bas_fe2_fr 0.191844756
+#define def_bas_mg_fr 0.166633099
+#define def_bas_ca_fr 0.189919663
+#define def_bas_na_fr 0.111927114
+#define def_bas_k_fr 0.021500819
